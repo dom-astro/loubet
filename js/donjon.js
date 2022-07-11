176da9d2eac1659f695906fdd693a01fe70584d6
Dom Astro <94188743+dom-astro@users.noreply.github.com>
2022-07-11 17:00:38 +0200

    Création

---
+++ js/donjon.js
@@ -0,0 +1,25 @@
+function write(id) {
+    jsonParagraphe="paragraphes/p"+id+".json";
+    $("#texte").empty();
+
+    $.getJSON(jsonParagraphe, function(data, status) {
+        console.info(data.texte.length);
+        $.each(data.texte, function(i, texte) {
+            var strParagraphe="<p>";
+            console.info(texte);
+
+            $.each(texte.paragraphe, function(i, ligne) {
+                strParagraphe += ligne+" ";
+            });
+
+            $("#texte").append(strParagraphe+"</p>");
+        });
+
+        $("#action").empty();
+        $.each(data.choix, function(i, choix){
+            $("#action").append('<p> => Rendez-vous au paragraphe <a href="javascript:paragraphe('+choix.id+')">'+choix.id+'</a></p>');
+        });
+    });
+}
+
+write(1);
